3981d82bf5ca99a9c2d62aad2cb8975154f9618a
Knar Gocer <knargr@gmail.com>
2020-10-27 17:14:45 +0000

    Automatically backed up by Learn

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,17 @@
 // your Bomb code here!
+
+import React from 'react';
+ 
+export default class Bomb extends Component{
+    constructor(props) {
+        super()
+        this.state = {secondsLeft : props.initialCount}
+      }
+      
+      render() {
+        const first = '<h1>Boom!</h1>';
+        const second = '<h1>{this.secondsLeft} seconds left before I go boom!</h1>';
+        
+        return(this.secondsLeft===0?first:second)          
+    }
+}
